895c8239dd8516d1e922a54ffa593797d6febefa
Eliana <elianacalote@gmail.com>
2022-02-09 21:27:42 +0000

    mapping through each square/refactoring

--- game/src/Components/Game/index.js
+++ game/src/Components/Game/index.js
@@ -1,13 +1,18 @@
-import React from 'react';
+import React, {useState} from 'react';
+import { calculateWinner } from '../helpers';
 import Board from '../Board';
 
 export default function Game () {
+    const [board, setBoard] = useState(Array(9).fill(null));
+    const [xIsNext, setXisNext] = useState(true);
+    const winner = calculateWinner(board);
+
    function handleClick () {
     
    }
 
     return (
-        <Board onClick={handleClick}/>
+        <Board squares={board} onClick={handleClick}/>
     )
     
 }
