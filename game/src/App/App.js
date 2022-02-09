fbee34c5286ac1fc785eba7e95d383b6844d28ca
Eliana <elianacalote@gmail.com>
2022-02-09 16:11:36 +0000

    TicTacToe component structure

---
+++ game/src/App/App.js
@@ -0,0 +1,19 @@
+
+import './App.css';
+import Game from '../Game';
+import Board from '../Board';
+import Square from '../Square';
+
+function App() {
+  return (
+    <div className="App">
+      <p>Hello</p>
+      <Game/>
+      <Board/>
+      <Square/>
+
+    </div>
+  );
+}
+
+export default App;
